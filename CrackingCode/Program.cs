cf93682a25836033a5a5faf448795c36425fc572
Darius Dubose <ddubose93@gmail.com>
2022-03-05 12:59:19 -0500

    Added SingleLinkedList Class which I implemented.

--- CrackingCode/Program.cs
+++ CrackingCode/Program.cs
@@ -7,14 +7,7 @@ namespace CrackingCode
     {
         static void Main(string[] args)
         {
-            var a1 = ArraysAndStrings.StringCompression("abcd");
-            var a2 = ArraysAndStrings.StringCompression("aabbccdd");
-            var a3 = ArraysAndStrings.StringCompression("aaabbbcccddd");
-            var a4 = ArraysAndStrings.StringCompression("aaabbbcccddd");
-            Console.WriteLine(a1);
-            Console.WriteLine(a2);
-            Console.WriteLine(a3);
-            Console.WriteLine(a4);
+            
         }
     }
 }
